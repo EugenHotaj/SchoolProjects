7941ce4a1bf529d99de1d46f402c58ffa0fd6e2d
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2016-01-29 13:46:07 -0500

    Vectorized emails

--- Machine Learning/Perceptron/Perceptron.py
+++ Machine Learning/Perceptron/Perceptron.py
@@ -1,6 +1,6 @@
 train_file = open("spam_train.txt");
 
-# Split up training data
+### Split up training data ###
 train_set = [];
 validate_set = [];
 
@@ -10,7 +10,7 @@ for i in range (0,5000):
     else:
         validate_set.append(train_file.readline());
 
-# Build Dictionary
+### Build Dictionary ###
 dictionary = {};
 
 for i in range (0,len(train_set)):
@@ -28,6 +28,29 @@ for i in range (0,len(train_set)):
 dictionary = {key:value for key, value in dictionary.items()
               if value >= 30};
 
+vector_location = 0;
+for key in dictionary:
+    dictionary[key] = vector_location;
+    vector_location += 1;
+
+### Transform data into vectors ###
+train_vectors = [];
+validate_vectors = [];
+
+def transform_to_vector(string):
+    v = [0] * len(dictionary);
+    current_string = string.split();
+    for i in range(0,len(current_string)):
+        word = current_string[i];
+        if(word in dictionary):
+            v[dictionary[word]] = 1;
+    return v;
+
+for i in range(0,len(train_set)):
+    train_vectors.append(transform_to_vector(train_set[i]));
+
+for i in range(0, len(validate_set)):
+    validate_vectors.append(transform_to_vector(validate_set[i]));
 
 
 input();
