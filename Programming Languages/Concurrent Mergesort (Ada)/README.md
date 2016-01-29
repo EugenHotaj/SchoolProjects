28f3bea7c6d3482379c9ce4de89f555a8c6bc1c8
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2016-01-29 17:40:51 -0500

    Removing old files

--- Programming Languages/Concurrent Mergesort (Ada)/README.md
+++
@@ -1,1 +0,0 @@
-Concurrent mergesort written in the Ada programming language. Mostly to gain experience with concurrency and imperative programming languages.
--- Programming Languages/Concurrent Mergesort (Ada)/sort.ads
+++
@@ -1,8 +0,0 @@
-package Sort is
-   SIZE : constant Integer := 40;
-   subtype AllowableRange is Integer range -500 .. 500;
-   subtype IndexRange is Integer range 1 .. SIZE;
-   type Arr is array (1 .. SIZE) of AllowableRange;
-   procedure MergeSort(InArr : in out Arr; Lo : in IndexRange; Hi : in IndexRange);
-end Sort;
-   
