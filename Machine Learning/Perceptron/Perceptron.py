0deb2edc7bb08bddb035df481b4716d82a0abec9
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2016-02-01 14:44:23 -0500

    Small changes.

--- Machine Learning/Perceptron/Perceptron.py
+++ Machine Learning/Perceptron/Perceptron.py
@@ -6,13 +6,17 @@ test_file = open("spam_test.txt");
 ### Split up training data ###
 train_set = [];
 validate_set = [];
+combined_set = [];
 
 for i in range (0,5000):
+    current_line = train_file.readline();
+    combined_set.append(current_line);    
+    
     if(i < 4000) :
-        train_set.append(train_file.readline());
+        train_set.append(current_line);
     else:
-        validate_set.append(train_file.readline());
-
+        validate_set.append(current_line);
+    
 ### Build Dictionary ###
 dictionary = {};
 default_class = 1;
@@ -41,6 +45,7 @@ for key in dictionary:
 ### Transform data into vectors ###
 train_vectors = [];
 validate_vectors = [];
+combined_vectors = [];
 
 def transform_to_vector(string):
     v = [0] * len(dictionary);
@@ -57,16 +62,22 @@ for i in range(0,len(train_set)):
 
 for i in range(0, len(validate_set)):
     validate_vectors.append(transform_to_vector(validate_set[i]));
+    
+combined_vectors.extend(train_vectors);
+combined_vectors.extend(validate_vectors);
 
 ### Perceptron Functions ###
-def perceptron_train(data):
+def perceptron_train(data, max_it):
+    if(max_it == 0):
+        max_it = 1000000;
+
     w = [0] * len(dictionary);
     k = 0;
     it = 0;
 
     while(True):
         flag = False;
-        w_old = list(w);
+        
         for i in range(0,len(data)):
             current_vector = data[i]['v'];
             current_spam = 1 if data[i]['spam'] == 1 else -1;
@@ -84,7 +95,7 @@ def perceptron_train(data):
         
         it += 1;
         
-        if(not flag or np.array_equal(w_old,w)):
+        if(not flag or it == max_it-1):
             break;
 
     return(w,k,it);
@@ -108,6 +119,16 @@ def perceptron_test(w,data):
     
     return (k/len(data));
 
-wv = perceptron_train(train_vectors);
-print(perceptron_test(wv[0],train_vectors));
-print(perceptron_test(wv[0],validate_vectors));
+print(perceptron_test(perceptron_train(train_vectors,0)[0],train_vectors));
+
+### Inspect Parameters ###
+
+### Averaged Perceptron ###
+def perceptron_train_avereged(data, max_it):
+    if(max_it == 0):
+        max_it = 1000000;
+    
+    return;
+    
+### Plots ###
+
