99c34da921b95195ee04723626977ec2c26918c2
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2016-02-03 17:43:30 -0500

    Hw stuff

--- Machine Learning/Perceptron/Perceptron.py
+++ Machine Learning/Perceptron/Perceptron.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt;
 train_file = open("spam_train.txt");
 test_file = open("spam_test.txt");
 
-### Split up training data ###
+### Read data, split up training data ###
 train_set = [];
 validate_set = [];
 combined_set = [];
+test_set = [];
 
 for i in range (0,5000):
     current_line = train_file.readline();
@@ -17,6 +18,9 @@ for i in range (0,5000):
         train_set.append(current_line);
     else:
         validate_set.append(current_line);
+
+test_set = test_file.readlines();
+
     
 ### Build Dictionary ###
 dictionary = {};
@@ -49,6 +53,7 @@ for key in dictionary:
 train_vectors = [];
 validate_vectors = [];
 combined_vectors = [];
+test_vectors = [];
 
 def transform_to_vector(string):
     v = [0] * len(dictionary);
@@ -65,9 +70,12 @@ for i in range(0,len(train_set)):
 
 for i in range(0, len(validate_set)):
     validate_vectors.append(transform_to_vector(validate_set[i]));
-    
+
 combined_vectors.extend(train_vectors);
 combined_vectors.extend(validate_vectors);
+
+for i in range(0, len(test_set)):
+    test_vectors.append(transform_to_vector(test_set[i]));
 
 ### Perceptron Functions ###
 def perceptron_train(data, max_it):
@@ -99,7 +107,7 @@ def perceptron_train(data, max_it):
         
         it += 1;
 
-        if(not flag or it == max_it-1):
+        if(not flag or it == max_it):
             break;
 
     return(w,k,it);
@@ -172,20 +180,20 @@ def perceptron_train_avereged(data, max_it):
         
         it += 1;
 
-        if(not flag or it == max_it-1):
+        if(not flag or it == max_it):
             break;    
     
     return(np.divide(w_total,it*len(data)),k,it);
 
 #### Question 7 & 8 ###
-#def train_on_set(data,N):
-#    p_n = perceptron_train(data[0:N],0);    
-#    p_a = perceptron_train_avereged(data[0:N],0);
-#    
-#    e_n = perceptron_test(p_n[0],validate_vectors);
-#    e_a = perceptron_test(p_a[0],validate_vectors);
-#    
-#    return ((e_n,p_n[2]),(e_a,p_a[2]));
+def train_on_set(data,N):
+    p_n = perceptron_train(data[0:N],0);    
+    p_a = perceptron_train_avereged(data[0:N],0);
+    
+    e_n = perceptron_test(p_n[0],validate_vectors);
+    e_a = perceptron_test(p_a[0],validate_vectors);
+    
+    return ((e_n,p_n[2]),(e_a,p_a[2]));
 #
 #plt_err_n = [];
 #plt_err_a = [];
@@ -241,4 +249,16 @@ def perceptron_train_avereged(data, max_it):
 #plt.scatter(np.array(plt_it)[:,0],np.array(plt_it)[:,1]);
 
 ### Question 10 ###
+#res = perceptron_train_avereged(train_vectors,0);
+#it = res[2];
+#print(perceptron_test(res[0],validate_vectors));
 
+#for i in range(0,it):
+#    wv = perceptron_train(train_vectors,it-i)[0];
+#    print(perceptron_test(wv,validate_vectors));
+#
+#for i in range(0,it+1):
+#    wv = perceptron_train_avereged(train_vectors,it-i)[0];
+#    print(perceptron_test(wv,validate_vectors));
+
+print(perceptron_test(perceptron_train_avereged(combined_vectors,0)[0],test_vectors));
